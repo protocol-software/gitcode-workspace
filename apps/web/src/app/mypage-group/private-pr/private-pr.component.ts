18b9b0013d8d76560e22c05132c73902169ecf09
techroad2019 <58165389+techroad2019@users.noreply.github.com>
2020-04-21 15:29:40 +0900

    Mypage-Private PR
    .

--- apps/web/src/app/mypage-group/private-pr/private-pr.component.ts
+++ apps/web/src/app/mypage-group/private-pr/private-pr.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-private-pr',
   templateUrl: './private-pr.component.html',
-  styleUrls: ['./private-pr.component.css']
+  styleUrls: ['./private-pr.component.scss']
 })
 export class PrivatePrComponent implements OnInit {
 

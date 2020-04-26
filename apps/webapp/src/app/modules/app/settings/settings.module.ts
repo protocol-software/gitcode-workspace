f12fed8e9dd194216796afefab5e51237db9c3eb
spock-mark1 <davidkang200@gmail.com>
2020-04-27 00:12:38 +0900

    add side-nave on settings

--- apps/webapp/src/app/modules/app/settings/settings.module.ts
+++ apps/webapp/src/app/modules/app/settings/settings.module.ts
@@ -3,9 +3,17 @@ import { RouterModule} from '@angular/router';
 import { SharedModule } from '../../../shared/shared.module';
 import { SettingsRoutes} from './settings.routing';
 import { SettingsComponent } from './settings.component';
+import {SideNavComponent} from './side-nav/side-nav.component';
+import {ProfileComponent} from './profile/profile.component';
+import {BillingComponent} from './billing/billing.component';
 
 @NgModule({
-  declarations: [SettingsComponent],
+  declarations: [
+    SettingsComponent,
+    SideNavComponent,
+    ProfileComponent,
+    BillingComponent,
+  ],
   imports: [
     RouterModule.forChild(SettingsRoutes),
     SharedModule,

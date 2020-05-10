55a091c77ef8944cb2d5a98c850bcc997c766fa2
spock-mark1 <davidkang200@gmail.com>
2020-05-10 20:47:56 +0900

    Create snack-code.service.ts

---
+++ apps/webapp/src/app/services/snack-code.service.ts
@@ -0,0 +1,29 @@
+import { Observable, throwError } from 'rxjs';
+import {HttpService} from './http.service';
+import * as SafeUrlAssembler from 'url-assembler';
+import {environment} from '../../environments/environment';
+import {Injectable} from '@angular/core';
+import {ApiEndpoint} from '../../../../../libs/data/src/lib/configurations/api-endpoint';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class OtpService {
+    constructor(private httpService: HttpService) {
+
+    }
+
+    public getTags<T>(): Observable<T> {
+        const endpoint = SafeUrlAssembler(ApiEndpoint.snackCode.getTags)
+            .toString();
+
+        return this.httpService.get<any>(endpoint);
+    }
+
+    public getContents<T>(data: any): Observable<T> {
+        const endpoint = SafeUrlAssembler(ApiEndpoint.snackCode.getContents)
+            .toString();
+
+        return this.httpService.post<any>(endpoint, data);
+    }
+}

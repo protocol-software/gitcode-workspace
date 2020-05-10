419f11602cfa10bb94993d949e3da9bf35e87c43
GangMinSeo <rivermin.seo@gmail.com>
2020-05-10 22:01:37 +0900

    UI 작업중

--- apps/webapp/src/app/modules/app/snack-code/list/list.component.ts
+++ apps/webapp/src/app/modules/app/snack-code/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {Router} from '@angular/router';
 
 @Component({
@@ -6,11 +7,36 @@ import {Router} from '@angular/router';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
-
-  constructor(private _router: Router) { }
+export class ListComponent implements OnInit {  
+  public checkedCategories = [];  
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    ) { 
+    this._route.queryParams.subscribe(params => {
+      const categoryItems = params['categoryItems'];
+      this.checkedCategories = (categoryItems) ? categoryItems.split(',') : [];      
+    })
+  }
 
   ngOnInit(): void {
+
+  }
+
+  public toggleCateItem (cateItem) {    
+    const checkedIndex = this.checkedCategories.indexOf(cateItem)
+    if(checkedIndex > -1) {
+      this.checkedCategories.splice(checkedIndex, 1)
+    }else{
+      this.checkedCategories.push(cateItem)
+    }    
+    let queryParams = {}
+    if(this.checkedCategories.length > 0) {      
+      queryParams = {
+        categoryItems: this.checkedCategories.toString()
+      }      
+    }
+    this._router.navigate([`/snack-code`], {queryParams: queryParams})
   }
 
 }
--- apps/webapp/src/app/modules/app/snack-code/side-nav/side-nav.component.ts
+++ apps/webapp/src/app/modules/app/snack-code/side-nav/side-nav.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {categoryList} from '../snack-code.data';
+import { fromEventPattern } from 'rxjs';
 
 @Component({
   selector: 'protocol-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent implements OnInit {
-
-  categoryList = categoryList;
-  constructor(private _router: Router) { }
-
-  ngOnInit(): void {
+export class SideNavComponent implements OnInit {    
+  private checkedCategories = []
+  public categoryOptions = [];    
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router) {      
+      _route.queryParams.subscribe(params => {
+        const categoryItems = params['categoryItems']
+        this.checkedCategories = (categoryItems) ? categoryItems.split(',') : []
+        this.initCategories()
+      })
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  private initCategories () { 
+    this.categoryOptions = []   
+    for(let item of categoryList) {
+      for(let subItem of item.subCategoryList) {        
+        subItem.checked = (this.checkedCategories.indexOf(subItem.name) > -1)
+      }
+      this.categoryOptions.push(item)
+    }
+  }
+
+  public toggleCateItem (cateItem) {    
+    const checkedIndex = this.checkedCategories.indexOf(cateItem)
+    if(checkedIndex > -1) {
+      this.checkedCategories.splice(checkedIndex, 1)
+    }else{
+      this.checkedCategories.push(cateItem)
+    }    
+    let queryParams = {}
+    if(this.checkedCategories.length > 0) {      
+      queryParams = {
+        categoryItems: this.checkedCategories.toString()
+      }      
+    }
+    this._router.navigate([`/snack-code`], {queryParams: queryParams})
+  }
 }

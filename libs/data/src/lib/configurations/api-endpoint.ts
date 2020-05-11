ce3406ce024b81bfacd9c2f298d3d74cba71610e
techroad2019 <58165389+techroad2019@users.noreply.github.com>
2020-05-12 03:59:53 +0900

    snack code detail
    .

--- libs/data/src/lib/configurations/api-endpoint.ts
+++ libs/data/src/lib/configurations/api-endpoint.ts
@@ -1,11 +1,11 @@
 export const ApiEndpoint = {
   globalPrefix: 'api',
   snackCode: {
-    getTags: `/api/snack-code/tags`,
-    getContents: `/api/snack-code/contents`,
+    getTags: `/snack-code/tags`,
+    getContents: `/snack-code/contents`,
   },
   searchCode: {
-    getSuggestQuery: `/api/search-code/suggest-query`,
-    query: `/api/search-code/query`,
+    getSuggestQuery: `/search-code/suggest-query`,
+    query: `/search-code/query`,
   },
 };

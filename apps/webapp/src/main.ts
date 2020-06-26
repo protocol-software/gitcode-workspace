818edffa5e3b96d18fb75f8504ba23c74806fc6f
techroad2019 <58165389+techroad2019@users.noreply.github.com>
2020-06-26 18:50:46 +0900

    done: favicon dark mode light mode

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -11,3 +11,35 @@ if ( environment.production )
 
 platformBrowserDynamic().bootstrapModule(AppModule)
                         .catch(err => console.error(err));
+
+// begin-favicon dark light mode
+function setupIcons() {
+    const lightSchemeIcon = document.querySelector('link#light-scheme-icon');
+    const darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+
+    function setLight() {
+        document.head.append(lightSchemeIcon);
+        darkSchemeIcon.remove();
+    }
+
+    function setDark() {
+        lightSchemeIcon.remove();
+        document.head.append(darkSchemeIcon);
+    }
+
+
+    const matcher = window.matchMedia('(prefers-color-scheme:dark)');
+    function onUpdate() {
+        if (matcher.matches) {
+            setDark();
+        } else {
+            setLight();
+        }
+    }
+    matcher.addListener(onUpdate);
+    onUpdate();
+}
+
+setupIcons();
+
+// end-favicon dark light mode

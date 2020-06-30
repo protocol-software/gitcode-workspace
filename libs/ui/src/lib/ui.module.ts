227560946472d99b6406d03afee55ecf17d157ff
Kiang <aesopturtle@msn.com>
2020-07-01 03:00:31 +0700

    feat: Add snack-copy-button component.

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,21 +2,30 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClipboardModule } from 'ngx-clipboard';
 import { ButtonComponent } from './buttons/regular/button.component';
+import { SnackCopyButtonComponent } from './buttons/snack-copy/snack-copy-button.component';
 import { ColorsComponent } from './colors/colors.component';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
     CommonModule,
     HttpClientModule,
     MatIconModule,
+    MatInputModule,
+    ClipboardModule,
   ],
   declarations: [
     ButtonComponent,
     ColorsComponent,
+    SnackCopyButtonComponent,
   ],
   exports: [
     ButtonComponent,
+    SnackCopyButtonComponent,
   ],
 })
 export class UiModule {}

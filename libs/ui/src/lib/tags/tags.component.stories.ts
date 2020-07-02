c304ed4f69f8c209f6f480ddf43865fa1613d84e
Kiang <aesopturtle@msn.com>
2020-07-03 01:50:56 +0700

    Add tag component.

---
+++ libs/ui/src/lib/tags/tags.component.stories.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { radios, select, text } from '@storybook/addon-knobs';
+import { TagComponent } from './regular/tag.component';
+
+export default {
+  title: 'Tags',
+};
+
+export const regular = () => ({
+  moduleMetadata: {
+    imports: [
+      CommonModule,
+      HttpClientModule,
+      MatIconModule,
+    ],
+    declarations: [TagComponent],
+  },
+  component: TagComponent,
+  template: `<gitcode-tag [type]='type' 
+                           [shouldAllowDismiss]='shouldAllowDismiss'>{{ text }}</gitcode-tag>`,
+  props: {
+    text: text('text', 'Awesome Tag'),
+    type: select(
+      'type',
+      ['none', 'open', 'review', 'reopen', 'closed', 'matching', 'matched', 'best', 'grade'],
+      'none'),
+    shouldAllowDismiss: radios('shouldAllowDismiss', { true: 'true', false: 'false' }, 'false'),
+  },
+});

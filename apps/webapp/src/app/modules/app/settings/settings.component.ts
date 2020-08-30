283a17889e36302864a602a271fa153c51c0999b
Kiang <aesopturtle@msn.com>
2020-08-30 23:57:00 +0700

    feat: Bind data to user block on Settings page.

--- apps/webapp/src/app/modules/app/settings/settings.component.ts
+++ apps/webapp/src/app/modules/app/settings/settings.component.ts
@@ -1,21 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { AngularFirestore, QueryFn } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { ICodeReviewItem, IUser } from '@gitcode/data';
+import { forkJoin, Observable } from 'rxjs';
+import { map, retry, take } from 'rxjs/operators';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'gitcode-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  @HostBinding('class') public hostClass = 'settings';
+
+  public user: IUser;
+  public publicRepos: ICodeReviewItem[];
+  public privateRepos: ICodeReviewItem[];
 
   activePath: string;
-  constructor(route: ActivatedRoute) {
+
+  constructor(route: ActivatedRoute,
+              private authService: AuthService,
+              private angularFirestore: AngularFirestore,
+  ) {
     route.params.subscribe((params) => {
       this.activePath = params['activePath'];
     });
+
+    this.authService.user$
+        .subscribe((user: IUser) => {
+          this.user = user;
+
+          forkJoin([
+            this.getRepositories(),
+            this.getRepositories(false),
+          ]).subscribe(
+            (repos) => {
+              this.publicRepos = repos[0];
+              this.privateRepos = repos[1];
+            },
+          );
+        });
   }
 
   ngOnInit(): void {
   }
 
+  private getRepositories(isPublic = true): Observable<ICodeReviewItem[]> {
+    const type = isPublic ? 'public' : 'private';
+    const query: QueryFn = (ref) => ref
+      .where('author.uid', '==', this.user?.uid);
+    return this.angularFirestore
+               .collection(`${type}-code-review`, query)
+               .snapshotChanges()
+               .pipe(
+                 retry(2),
+                 take(1),
+                 map(docs => docs.map(doc => doc.payload.doc.data() as ICodeReviewItem)),
+               );
+  }
 }
--- apps/webapp/src/app/modules/app/settings/side-nav/side-nav.component.ts
+++ apps/webapp/src/app/modules/app/settings/side-nav/side-nav.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '@gitcode/data';
 
 @Component({
   selector: 'gitcode-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
+  @HostBinding('class') public hostClass = 'side-nav';
+
+  @Input() public user: IUser;
+  @Input() public repoCount: number;
 
   constructor(private _router: Router) { }
 
   ngOnInit(): void {
   }
-
 }
